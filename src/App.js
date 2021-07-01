dc549facaae48148c7479d72bf2976595d364750
jeonghoon jeon <shu7081@gmail.com>
2021-07-02 05:34:22 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [title, chTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 강의",
+  ]);
+  let [good, chGood] = useState(0);
+  let [modal, chModal] = useState(true);
+  let [inputNumber, chInputNumber] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+
+      {title.map(function (title, i) {
+        return (
+          <div className="list">
+            <h3
+              onClick={() => {
+                chInputNumber(i);
+              }}
+            >
+              {title}
+              <span
+                onClick={() => {
+                  chGood(good + 1);
+                }}
+              >
+                👍
+              </span>
+              {good}
+            </h3>
+            <p>6월 29일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      {modal === true ? (
+        <Modal title={title} inputNumber={inputNumber} />
+      ) : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h2>{props.title[props.inputNumber]}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
